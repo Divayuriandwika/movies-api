09d58354ab43d681e39986709b604cdf5ef9ca1d
Diva Yurian DWika <divayuriandwika@gmail.com>
2020-07-28 12:10:13 +0700

    create backend files (config, models, routes etc) and connecting to mongo's database

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { PORT, db } = require('./config');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome to movies api ' });
+});
+
+
+if (db) {
+	app.listen(PORT, () => {
+		console.log(`this app listen to port ${PORT}`)
+	});
+}
